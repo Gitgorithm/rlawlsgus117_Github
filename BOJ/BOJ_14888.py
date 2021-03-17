5338db8b69ab515ab058907b146bebb061180bc2
Kimjinhyeon0 <rlawlsgus117@gmail.com>
2021-03-17 14:13:51 +0900

    BOJ_Day3

---
+++ BOJ/BOJ_14888.py
@@ -0,0 +1,9 @@
+import sys
+from itertools import combinations
+
+n = int(sys.stdin.readline())
+num = list(map(int, sys.stdin.readline().split()))
+cal = list(map(int, sys.stdin.readline().split()))
+
+
+print(n, num, cal, sep='\n')
