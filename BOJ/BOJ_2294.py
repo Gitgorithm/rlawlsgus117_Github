0af194ec374d2b27dadd6472d33c22b0fc23a907
Kimjinhyeon0 <rlawlsgus117@gmail.com>
2021-05-15 23:32:02 +0900

    BOJ_Day9

---
+++ BOJ/BOJ_2294.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+coins = []
+dp = [10002 for _ in range(k+1)]
+for _ in range(n):
+    coin = int(input())
+    if coin <= k:
+        coins.append(coin)
+        dp[coin] = 1
+for i in range(k+1):
+    for coin in coins:
+        print(dp)
+        if i>= coin:
+            dp[i] = min(dp[i], dp[i-coin]+1) # 10002,.
+
+print(dp)
+# if dp[k] == 10002:
+#     print(-1)
+# else:
+#     print(dp[k])
